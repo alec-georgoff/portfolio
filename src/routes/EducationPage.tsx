cf60f8a24a94eaddddb328b195d3a98fc39fc99c
Alec Georgoff <alec.georgoff@gmail.com>
2023-06-23 23:46:01 -0700

    Update Education page.

--- src/routes/EducationPage.tsx
+++ src/routes/EducationPage.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import UMLogo from '../images/um_logo.png';
 
 const EducationPage = () => (
     <div>
         <h2>Education</h2>
 
-        <section className="job">
-            <div className="job-title-and-date-row">
-                <div className="job-title">B.S. - Biomedical Engineering</div>
-                <div className="job-dates">Sep 13 - May 17</div>
+        <img src={UMLogo} className="um-logo" />
+
+        <section className="education">
+            <div className="education-degree">
+                B.S. - Biomedical Engineering
             </div>
-            <div className="job-company-name">
+            <div className="education-dates">Sep '13 - May '17</div>
+            <div className="education-school-name">
                 <a href="https://umich.edu">University of Michigan</a>
             </div>
-            <div className="job-location">Ann Arbor, MI</div>
+            <div className="education-location">Ann Arbor, MI</div>
         </section>
     </div>
 );
