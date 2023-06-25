3ee892ac1e6bf5434e2e663712327aae027f34f3
Alec Georgoff <alec.georgoff@gmail.com>
2023-06-24 23:43:25 -0700

    Implement image refresh on click.

--- src/integrations/smugmug/SmugmugWidget.tsx
+++ src/integrations/smugmug/SmugmugWidget.tsx
@@ -1,30 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { getAlbumKeys, getRandomImageUrl } from './service';
 
 export const SmugmugWidget = () => {
     const [albumKeys, setAlbumKeys] = useState<string[]>([]);
     const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         console.log(1);
-        getAlbumKeys()
-            .then((res) => setAlbumKeys(res))
-            .catch((err) => console.log(err));
-    }, []);
-
-    useEffect(() => {
         void (async () => {
-            const url = await getRandomImageUrl(albumKeys);
+            const keys = await getAlbumKeys();
+            setAlbumKeys(keys);
+
+            const url = await getRandomImageUrl(keys);
             setImageUrl(url);
+            setLoading(false);
         })();
 
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         return () => {};
-    }, [albumKeys]);
+    }, []);
+
+    const refreshImage = useCallback(() => {
+        console.log('refresh');
+        getRandomImageUrl(albumKeys)
+            .then((res) => {
+                setImageUrl(res);
+                setLoading(false);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    const handleRefreshImage = () => {
+        setImageUrl(undefined);
+        refreshImage();
+    };
 
     return (
         <div>
-            {imageUrl ? <img src={imageUrl} /> : <span>Loading image</span>}
+            {imageUrl && !loading ? (
+                <img src={imageUrl} onClick={() => handleRefreshImage()} />
+            ) : (
+                <span>Loading image</span>
+            )}
         </div>
     );
 };
